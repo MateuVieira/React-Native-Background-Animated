f2370a103b98c364031281a78bd7e74a8398883f
Mateus Vieira <mateus17v@gmail.com>
2020-05-03 19:01:30 -0300

    Add configs constants

---
+++ config/Constants.js
@@ -0,0 +1,7 @@
+// Constants of Component BackgroundAnimation
+export const INPUT_RANGE_START = 0;
+export const INPUT_RANGE_END = 1;
+export const OUTPUT_RANGE_START = -281;
+export const OUTPUT_RANGE_END = 0;
+export const ANIMATION_TO_VALUE = 1;
+export const ANIMATION_DURATION = 25000; // 
